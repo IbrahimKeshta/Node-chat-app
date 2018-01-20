e250794b470084318c288f1a031ae4f2c859a47b
Ibrahim Keshta <keshta95@hotmail.com>
2018-01-21 01:46:34 +0200

    change ip

--- server/server.js
+++ server/server.js
@@ -47,6 +47,6 @@ io.on('connection', (socket) => {
 }); 
 app.use(express.static(publicPath));
 
-server.listen(port,'127.0.0.1', (e) => {
+server.listen(port, (e) => {
     console.log(`Server is up on port ${port}`);
 });
